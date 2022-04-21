041cbac266b7399e4d01ae4b06917de72e321619
gabriel velez <gabriel.dan.velez@gmail.com>
2022-04-21 14:44:19 -0400

    .

--- src/state/action-creators.js
+++ src/state/action-creators.js
@@ -7,6 +7,7 @@ export const fetchRandomActivityFromApi = () => (dispatch) => {
     .then((res) => {
       const activityFromApi = res.data.activity;
       dispatch({ type: types.POPULATE_ACTIVITY, payload: activityFromApi });
+      dispatch({ type: types.POPULATE_ERROR, payload: "" });
     })
     .catch((err) => {
       (err) => console.error(err);
